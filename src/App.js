da067117fcbe9b3e228fbd0528f5b202bd36928a
SaviHerath <savi@chaebanicecream.com>
2021-09-05 05:06:02 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx';
 import { Route, Switch, Link } from 'react-router-dom'
+import Header from './components/header/header.component';
+
 
 
 
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
         <Route path="/shop" component={ShopPage} />
         <Route exact path="/" component={HomePage} />

f3767c408255d8ccd0db6074e1c04b704f6f07bc
SaviHerath <savi@chaebanicecream.com>
2021-09-04 23:50:05 +0530

    Updating shoppage with collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx'
+import ShopPage from './pages/shop/shop.component.jsx';
 import { Route, Switch, Link } from 'react-router-dom'
 
-const HatsPage = (props) => {
-  const {history} =props
-  console.log(history)
-  return (
-    <div>
-      <Link to="/">GO TO HOME PAGE</Link>
-      <h1>THis is the test Page React</h1>
-    </div>
 
-  )
-}
 
 function App() {
   return (
     <div >
       <Switch>
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
         <Route exact path="/" component={HomePage} />
-
       </Switch>
 
     </div>

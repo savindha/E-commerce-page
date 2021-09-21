42024caf47db228e98e032ef6dd4ff6119a3ac16
SaviHerath <savi@chaebanicecream.com>
2021-09-21 23:41:56 +0530

    added cart selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import './cart-dropdown.scss'
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component';
 import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 
 const CartDropdown = ({ cartItems }) => (
@@ -20,8 +21,9 @@ const CartDropdown = ({ cartItems }) => (
 )
 
 const mapStateToProps = (state) => ({
-    cartItems: state.cart.cartItems
+    cartItems: selectCartItems(state)
 
 })
+
 
 export default connect(mapStateToProps)(CartDropdown)

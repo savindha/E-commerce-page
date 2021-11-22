569d0094916a4936c23526b7fc4024f677be2d2c
SaviHerath <savi@chaebanicecream.com>
2021-11-22 13:32:48 +0530

    Moved to a new folder

--- src/App.js
+++ src/App.js
@@ -12,16 +12,22 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {
+  auth, createUserProfileDocument,
+  // addCollectionAndDocuments
+} from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+// import { selectCollectionsForPreview } from './redux/shop/shop.selectors'  //  Should run only one time when app started 
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser,
+      // collectionsArray
+    } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -36,6 +42,7 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+      // addCollectionAndDocuments('collection', collectionsArray.map(({ title, items }) => ({ title, items }))) 
     });
   }
 
@@ -69,7 +76,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  // collectionsArray: selectCollectionsForPreview  //  Should run only one time when app started 
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyAsICmj1bJ3Mb5ZxYrMJVV3wq71fcqis6w",
+  authDomain: "crwn-db-5ce93.firebaseapp.com",
+  projectId: "crwn-db-5ce93",
+  storageBucket: "crwn-db-5ce93.appspot.com",
+  messagingSenderId: "964981204869",
+  appId: "1:964981204869:web:729e22774e00ab927cc6ec",
+  measurementId: "G-EM2PM8GNER"
 };
 
 firebase.initializeApp(config);
@@ -38,6 +38,40 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   return userRef;
 };
+
+
+export const addCollectionAndDocuments = async (collectionKey, objectToAdd) => {
+  const collectionRef = firestore.collection(collectionKey)
+  const batch = firestore.batch()
+
+  objectToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc()
+    batch.set(newDocRef, obj)
+  })
+
+  return await batch.commit()
+
+}
+
+
+export const convertCollectionsSanpshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  })
+
+  return transformedCollection.reduce((accumilator, collection) => {
+    accumilator[collection.title.toLowerCase()] = collection;
+    return accumilator
+  }, {})
+
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();

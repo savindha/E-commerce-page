bcef7181a2586e17874af470050d1b0c67c27f2e
SaviHerath <savi@chaebanicecream.com>
2021-12-07 20:57:31 +0530

    Resolved dep issue

---
+++ client/build/precache-manifest.07c17ad76aaed9c7cf5055c9379b7bf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc747d3511fb826f2e782c56482a287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "951404f3b95d8e9b85e7",
+    "url": "/static/css/main.b95fee0c.chunk.css"
+  },
+  {
+    "revision": "202ed09b0e5d02459fa8",
+    "url": "/static/js/2.694bb84b.chunk.js"
+  },
+  {
+    "revision": "951404f3b95d8e9b85e7",
+    "url": "/static/js/main.d40b44f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
